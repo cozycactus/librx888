ee57b4881bb88b090cb12d937634e1805ca4b3b3
Ruslan Migirov <trapi78@gmail.com>
2022-06-18 15:58:22 +0300

    Save

--- src/librx888.c
+++ src/librx888.c
@@ -6,7 +6,7 @@
 /*   By: Ruslan Migirov <trapi78@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/15 16:10:17 by Ruslan Migi       #+#    #+#             */
-/*   Updated: 2022/06/15 19:46:19 by Ruslan Migi      ###   ########.fr       */
+/*   Updated: 2022/06/18 15:56:15 by Ruslan Migi      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,11 +58,34 @@ static rx888_t *find_known_device(uint16_t vid, uint16_t pid)
 	return device;
 }
 
+uint32_t rtlsdr_get_device_count(void)
+{
+	libusb_context *ctx;
+	int r = libusb_init(&ctx);
+	if(r < 0)
+		return 0;
+
+	libusb_device **list;
+	ssize_t cnt = libusb_get_device_list(ctx, &list);
+
+	uint32_t device_count = 0;
+	struct libusb_device_descriptor dd;
+	for (int i = 0; i < cnt; i++) {
+		libusb_get_device_descriptor(list[i], &dd);
+
+		if (find_known_device(dd.idVendor, dd.idProduct))
+			device_count++;
+	}
+
+	libusb_free_device_list(list, 1);
+
+	libusb_exit(ctx);
+
+	return device_count;
+}
+
 int rx888_open(rx888_dev_t **out_dev, uint32_t index)
 {
-    
-    
-
     rx888_dev_t *dev = calloc(1, sizeof(rx888_dev_t));
     if (dev == NULL)
         return -ENOMEM;
@@ -84,11 +107,35 @@ int rx888_open(rx888_dev_t **out_dev, uint32_t index)
 
         libusb_get_device_descriptor(list[i], &dev_desc);
 
+        if (find_known_device(dev_desc.idVendor, dev_desc.idProduct)) {
+            device_count++;
+        }
 
+        if (index == device_count - 1)
+            break;
+        
+        device = NULL;
+    }
+
+    if(!device) {
+        r = -1;
+        goto err;
     }
 
     *out_dev = dev;
     return 0;
+err:
+	if (dev) {
+		if (dev->dev_handle)
+			libusb_close(dev->dev_handle);
+
+		if (dev->ctx)
+			libusb_exit(dev->ctx);
+
+		free(dev);
+	}
+
+	return r;
     
 }
 

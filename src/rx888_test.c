6904a7bedb2a36fe1d66d68cb34c9ef66c35161a
Ruslan Migirov <trapi78@gmail.com>
2022-08-20 22:03:32 +0300

    Add initial rx888 test utility

---
+++ src/rx888_test.c
@@ -0,0 +1,440 @@
+#include <errno.h>
+#include <signal.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#ifdef __APPLE__
+#include <sys/time.h>
+#else
+#include <time.h>
+#endif
+
+#ifndef _WIN32
+#include <unistd.h>
+#else
+#include <windows.h>
+#include "getopt/getopt.h"
+#endif
+
+#include "librx888.h"
+
+#define DEFAULT_SAMPLE_RATE		2048000
+#define DEFAULT_BUF_LENGTH      (1024 * 16 * 8)
+#define MINIMAL_BUF_LENGTH		1024
+#define MAXIMAL_BUF_LENGTH		(256 * 16384)
+
+#define MHZ(x)	((x)*1000*1000)
+
+#define PPM_DURATION			10
+#define PPM_DUMP_TIME			5
+
+struct time_generic
+/* holds all the platform specific values */
+{
+#ifndef _WIN32
+	time_t tv_sec;
+	long tv_nsec;
+#else
+	long tv_sec;
+	long tv_nsec;
+	int init;
+	LARGE_INTEGER frequency;
+	LARGE_INTEGER ticks;
+#endif
+};
+
+static enum {
+	NO_BENCHMARK,
+	TUNER_BENCHMARK,
+	PPM_BENCHMARK
+} test_mode = NO_BENCHMARK;
+
+static int do_exit = 0;
+static rx888_dev_t *dev = NULL;
+
+static uint32_t samp_rate = DEFAULT_SAMPLE_RATE;
+
+static uint32_t total_samples = 0;
+static uint32_t dropped_samples = 0;
+
+static unsigned int ppm_duration = PPM_DURATION;
+
+int verbose_set_sample_rate(rx888_dev_t *dev, uint32_t samp_rate)
+{
+	int r;
+	r = rx888_set_sample_rate(dev, samp_rate);
+	if (r < 0) {
+		fprintf(stderr, "WARNING: Failed to set sample rate.\n");
+	} else {
+		fprintf(stderr, "Sampling at %u S/s.\n", samp_rate);
+	}
+	return r;
+}
+
+
+int verbose_device_search(char *s)
+{
+	int i, device_count, device, offset;
+	char *s2;
+	char vendor[256], product[256], serial[256];
+	device_count = rx888_get_device_count();
+	if (!device_count) {
+		fprintf(stderr, "No supported devices found.\n");
+		return -1;
+	}
+	fprintf(stderr, "Found %d device(s):\n", device_count);
+	for (i = 0; i < device_count; i++) {
+		rx888_get_device_usb_strings(i, vendor, product, serial);
+		fprintf(stderr, "  %d:  %s, %s, SN: %s\n", i, vendor, product, serial);
+	}
+	fprintf(stderr, "\n");
+	/* does string look like raw id number */
+	device = (int)strtol(s, &s2, 0);
+	if (s2[0] == '\0' && device >= 0 && device < device_count) {
+		fprintf(stderr, "Using device %d: %s\n",
+			device, rx888_get_device_name((uint32_t)device));
+		return device;
+	}
+	/* does string exact match a serial */
+	for (i = 0; i < device_count; i++) {
+		rx888_get_device_usb_strings(i, vendor, product, serial);
+		if (strcmp(s, serial) != 0) {
+			continue;}
+		device = i;
+		fprintf(stderr, "Using device %d: %s\n",
+			device, rx888_get_device_name((uint32_t)device));
+		return device;
+	}
+	/* does string prefix match a serial */
+	for (i = 0; i < device_count; i++) {
+		rx888_get_device_usb_strings(i, vendor, product, serial);
+		if (strncmp(s, serial, strlen(s)) != 0) {
+			continue;}
+		device = i;
+		fprintf(stderr, "Using device %d: %s\n",
+			device, rx888_get_device_name((uint32_t)device));
+		return device;
+	}
+	/* does string suffix match a serial */
+	for (i = 0; i < device_count; i++) {
+		rx888_get_device_usb_strings(i, vendor, product, serial);
+		offset = strlen(serial) - strlen(s);
+		if (offset < 0) {
+			continue;}
+		if (strncmp(s, serial+offset, strlen(s)) != 0) {
+			continue;}
+		device = i;
+		fprintf(stderr, "Using device %d: %s\n",
+			device, rx888_get_device_name((uint32_t)device));
+		return device;
+	}
+	fprintf(stderr, "No matching devices found.\n");
+	return -1;
+}
+
+void usage(void)
+{
+	fprintf(stderr,
+		"rx888_test, a benchmark tool for rx888 receiver\n\n"
+		"Usage:\n"
+		"\t[-s samplerate (default: 2048000 Hz)]\n"
+		"\t[-d device_index (default: 0)]\n"
+		"\t[-t enable Elonics E4000 tuner benchmark]\n"
+#ifndef _WIN32
+		"\t[-p[seconds] enable PPM error measurement (default: 10 seconds)]\n"
+#endif
+		"\t[-b output_block_size (default: 16 * 16384)]\n"
+		"\t[-S force sync output (default: async)]\n");
+	exit(1);
+}
+
+#ifdef _WIN32
+BOOL WINAPI
+sighandler(int signum)
+{
+	if (CTRL_C_EVENT == signum) {
+		fprintf(stderr, "Signal caught, exiting!\n");
+		do_exit = 1;
+		rtlsdr_cancel_async(dev);
+		return TRUE;
+	}
+	return FALSE;
+}
+#else
+static void sighandler(int signum)
+{
+    (void)signum;
+	fprintf(stderr, "Signal caught, exiting!\n");
+	do_exit = 1;
+	rx888_cancel_async(dev);
+}
+#endif
+
+static void underrun_test(unsigned char *buf, uint32_t len, int mute)
+{
+	uint32_t i, lost = 0;
+	static uint8_t bcnt, uninit = 1;
+
+	if (uninit) {
+		bcnt = buf[0];
+		uninit = 0;
+	}
+	for (i = 0; i < len; i++) {
+		if(bcnt != buf[i]) {
+			lost += (buf[i] > bcnt) ? (buf[i] - bcnt) : (bcnt - buf[i]);
+			bcnt = buf[i];
+		}
+
+		bcnt++;
+	}
+
+	total_samples += len;
+	dropped_samples += lost;
+	if (mute)
+		return;
+	if (lost)
+		printf("lost at least %d bytes\n", lost);
+
+}
+
+#ifndef _WIN32
+static int ppm_gettime(struct time_generic *tg)
+{
+	int rv = ENOSYS;
+	//struct timespec ts;
+
+#ifdef __unix__
+	rv = clock_gettime(CLOCK_MONOTONIC, &ts);
+	tg->tv_sec = ts.tv_sec;
+	tg->tv_nsec = ts.tv_nsec;
+#elif __APPLE__
+	struct timeval tv;
+
+	rv = gettimeofday(&tv, NULL);
+	tg->tv_sec = tv.tv_sec;
+	tg->tv_nsec = tv.tv_usec * 1000;
+#endif
+	return rv;
+}
+#endif
+
+#ifdef _WIN32
+static int ppm_gettime(struct time_generic *tg)
+{
+	int rv;
+	int64_t frac;
+	if (!tg->init) {
+		QueryPerformanceFrequency(&tg->frequency);
+		tg->init = 1;
+	}
+	rv = QueryPerformanceCounter(&tg->ticks);
+	tg->tv_sec = tg->ticks.QuadPart / tg->frequency.QuadPart;
+	frac = (int64_t)(tg->ticks.QuadPart - (tg->tv_sec * tg->frequency.QuadPart));
+	tg->tv_nsec = (long)(frac * 1000000000L / (int64_t)tg->frequency.QuadPart);
+	return !rv;
+}
+#endif
+
+static int ppm_report(uint64_t nsamples, uint64_t interval)
+{
+	double real_rate, ppm;
+
+	real_rate = nsamples * 1e9 / interval;
+	ppm = 1e6 * (real_rate / (double)samp_rate - 1.);
+	return (int)round(ppm);
+}
+
+static void ppm_test(uint32_t len)
+{
+	static uint64_t nsamples = 0;
+	static uint64_t interval = 0;
+	static uint64_t nsamples_total = 0;
+	static uint64_t interval_total = 0;
+	struct time_generic ppm_now;
+	static struct time_generic ppm_recent;
+	static enum {
+		PPM_INIT_NO,
+		PPM_INIT_DUMP,
+		PPM_INIT_RUN
+	} ppm_init = PPM_INIT_NO;
+
+	ppm_gettime(&ppm_now);
+
+	if (ppm_init != PPM_INIT_RUN) {
+		/*
+		 * Kyle Keen wrote:
+		 * PPM_DUMP_TIME throws out the first N seconds of data.
+		 * The dongle's PPM is usually very bad when first starting up,
+		 * typically incorrect by more than twice the final value.
+		 * Discarding the first few seconds allows the value to stabilize much faster.
+		*/
+		if (ppm_init == PPM_INIT_NO) {
+			ppm_recent.tv_sec = ppm_now.tv_sec + PPM_DUMP_TIME;
+			ppm_init = PPM_INIT_DUMP;
+			return;
+		}
+		if (ppm_init == PPM_INIT_DUMP && ppm_recent.tv_sec < ppm_now.tv_sec)
+			return;
+		ppm_recent = ppm_now;
+		ppm_init = PPM_INIT_RUN;
+		return;
+	}
+
+	nsamples += (uint64_t)(len / 2UL);
+	interval = (uint64_t)(ppm_now.tv_sec - ppm_recent.tv_sec);
+	if (interval < ppm_duration)
+		return;
+	interval *= 1000000000UL;
+	interval += (int64_t)(ppm_now.tv_nsec - ppm_recent.tv_nsec);
+	nsamples_total += nsamples;
+	interval_total += interval;
+	printf("real sample rate: %i current PPM: %i cumulative PPM: %i\n",
+		(int)((1000000000UL * nsamples) / interval),
+		ppm_report(nsamples, interval),
+		ppm_report(nsamples_total, interval_total));
+	ppm_recent = ppm_now;
+	nsamples = 0;
+}
+
+static void rtlsdr_callback(unsigned char *buf, uint32_t len, void *ctx)
+{
+    (void)ctx;
+	underrun_test(buf, len, 0);
+
+	if (test_mode == PPM_BENCHMARK)
+		ppm_test(len);
+}
+
+int main(int argc, char **argv)
+{
+#ifndef _WIN32
+	struct sigaction sigact;
+#endif
+	int n_read, r, opt;
+	int sync_mode = 0;
+	uint8_t *buffer;
+	int dev_index = 0;
+	int dev_given = 0;
+	uint32_t out_block_size = DEFAULT_BUF_LENGTH;
+
+	while ((opt = getopt(argc, argv, "d:s:b:tp::Sh")) != -1) {
+		switch (opt) {
+		case 'd':
+			dev_index = verbose_device_search(optarg);
+			dev_given = 1;
+			break;
+		case 's':
+			samp_rate = (uint32_t)atof(optarg);
+			break;
+		case 'b':
+			out_block_size = (uint32_t)atof(optarg);
+			break;
+		case 't':
+			test_mode = TUNER_BENCHMARK;
+			break;
+		case 'p':
+			test_mode = PPM_BENCHMARK;
+			if (optarg)
+				ppm_duration = atoi(optarg);
+			break;
+		case 'S':
+			sync_mode = 1;
+			break;
+		case 'h':
+		default:
+			usage();
+			break;
+		}
+	}
+
+	if(out_block_size < MINIMAL_BUF_LENGTH ||
+	   out_block_size > MAXIMAL_BUF_LENGTH ){
+		fprintf(stderr,
+			"Output block size wrong value, falling back to default\n");
+		fprintf(stderr,
+			"Minimal length: %u\n", MINIMAL_BUF_LENGTH);
+		fprintf(stderr,
+			"Maximal length: %u\n", MAXIMAL_BUF_LENGTH);
+		out_block_size = DEFAULT_BUF_LENGTH;
+	}
+
+	buffer = malloc(out_block_size * sizeof(uint8_t));
+
+	if (!dev_given) {
+		dev_index = verbose_device_search("0");
+	}
+
+	if (dev_index < 0) {
+		exit(1);
+	}
+
+	r = rx888_open(&dev, (uint32_t)dev_index);
+	if (r < 0) {
+		fprintf(stderr, "Failed to open rtlsdr device #%d.\n", dev_index);
+		exit(1);
+	}
+#ifndef _WIN32
+	sigact.sa_handler = sighandler;
+	sigemptyset(&sigact.sa_mask);
+	sigact.sa_flags = 0;
+	sigaction(SIGINT, &sigact, NULL);
+	sigaction(SIGTERM, &sigact, NULL);
+	sigaction(SIGQUIT, &sigact, NULL);
+	sigaction(SIGPIPE, &sigact, NULL);
+#else
+	SetConsoleCtrlHandler( (PHANDLER_ROUTINE) sighandler, TRUE );
+#endif
+	
+	/* Set the sample rate */
+	verbose_set_sample_rate(dev, samp_rate);
+
+	
+
+	if ((test_mode == PPM_BENCHMARK) && !sync_mode) {
+		fprintf(stderr, "Reporting PPM error measurement every %u seconds...\n", ppm_duration);
+		fprintf(stderr, "Press ^C after a few minutes.\n");
+	}
+
+	if (test_mode == NO_BENCHMARK) {
+		fprintf(stderr, "\nInfo: This tool will continuously"
+				" read from the device, and report if\n"
+				"samples get lost. If you observe no "
+				"further output, everything is fine.\n\n");
+	}
+
+	if (sync_mode) {
+		fprintf(stderr, "Reading samples in sync mode...\n");
+		fprintf(stderr, "(Samples are being lost but not reported.)\n");
+		while (!do_exit) {
+			r = rx888_read_sync(dev, buffer, out_block_size, &n_read);
+			if (r < 0) {
+				fprintf(stderr, "WARNING: sync read failed.\n");
+				break;
+			}
+
+			if ((uint32_t)n_read < out_block_size) {
+				fprintf(stderr, "Short read, samples lost, exiting!\n");
+				break;
+			}
+			underrun_test(buffer, n_read, 1);
+		}
+	} else {
+		fprintf(stderr, "Reading samples in async mode...\n");
+		r = rx888_read_async(dev, rtlsdr_callback, NULL,
+				      0, out_block_size);
+	}
+
+	if (do_exit) {
+		fprintf(stderr, "\nUser cancel, exiting...\n");
+		fprintf(stderr, "Samples per million lost (minimum): %i\n", (int)(1000000L * dropped_samples / total_samples));
+	}
+	else
+		fprintf(stderr, "\nLibrary error %d, exiting...\n", r);
+
+	rx888_close(dev);
+	free (buffer);
+
+	return r >= 0 ? r : -r;
+}

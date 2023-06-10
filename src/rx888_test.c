242313df629b51cda2b1c54be4489e7b691d3135
Ruslan Migirov <trapi78@gmail.com>
2023-06-10 09:03:16 +0300

    Add copyright and licensing info

--- src/rx888_test.c
+++ src/rx888_test.c
@@ -1,3 +1,25 @@
+/*
+ * rx888_test.c - part of rx888 driver
+ * rx888_test.c is part of rx888 driver
+ *
+ * Copyright (C) 2012-2014 by Steve Markgraf <steve@steve-m.de>
+ * Copyright (C) 2012-2014 by Kyle Keen <keenerd@gmail.com>
+ * Copyright (C) 2014 by Michael Tatarinov <kukabu@gmail.com>
+ * Copyright (C) 2022-2023 by Ruslan Migirov <trapi78@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #define _POSIX_C_SOURCE 200112L
 
 #include <errno.h>

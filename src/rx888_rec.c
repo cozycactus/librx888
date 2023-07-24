832dea34de7885c3cc9015b9eb2bcfb5ca28c6ca
Ruslan Migirov <trapi78@gmail.com>
2023-07-24 19:05:31 +0300

    Fix linux build

--- src/rx888_rec.c
+++ src/rx888_rec.c
@@ -15,6 +15,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+#define _POSIX_C_SOURCE 200112L
 
 #include <errno.h>
 #include <signal.h>
